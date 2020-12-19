beed82c2629270a984daa5eb6a736f879a3e66e5
Aryan Srivastava <aryan@Aryans-MacBook-Air.local>
2020-12-19 18:47:01 +0530

    some more small fixes

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default class About extends React.Component {
     render(){ 
         return <div class='about-container'>
-             <h1>About me</h1>
+             <h1 class='section-header'>About me</h1>
             </div>;
     }
 }
