9ee2aecad9734b09bf9acad8f42f8a1d32e8ef8f
Aryan Srivastava <aryan@Aryans-MacBook-Air.local>
2021-08-30 18:47:53 -0400

    Updates

---
+++ precache-manifest.e0e08e9691011bd17b27da1f9951e5a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f696d1f767a8e058fc05",
+    "url": "/static/css/main.2141999d.chunk.css"
+  },
+  {
+    "revision": "f696d1f767a8e058fc05",
+    "url": "/static/js/main.db0b308a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dad715099510e31ffb49",
+    "url": "/static/css/2.0859a7e8.chunk.css"
+  },
+  {
+    "revision": "dad715099510e31ffb49",
+    "url": "/static/js/2.a95e4974.chunk.js"
+  },
+  {
+    "revision": "bf52b7b7160b613e3328d03fccb055b3",
+    "url": "/index.html"
+  }
+];
